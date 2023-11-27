7b5a787d65d0003b9d2c4cdef4dbb1111eb4a8b6
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-11-27 14:45:49 +0600

    change merge bugs

--- src/Components/Corporation/Corporation.tsx
+++ src/Components/Corporation/Corporation.tsx
@@ -1,24 +1,26 @@
-import './Corporation.css'
+import styles from './Corporation.module.css';
 import Form from 'react-bootstrap/Form';
-import {Button} from "react-bootstrap";
+import { Button } from 'react-bootstrap';
 
 const Corporation = () => {
-    // todo: исправить
     return (
-        <div className='footer-top'>
-            <div className={"footer-top-content"}>
+        <div className={styles.footerTop}>
+            <div className={styles.footerTopContent}>
                 <h1>Сотрудничество</h1>
-                <p>Если вы хотите стать нашим партнером, инвестором или заказчиком, то просим вас заполнить данную
-                    форму.</p>
-                <form className={"footer-top-input"}>
-                    <Form.Control type="email" placeholder="Почта" size={"lg"}/>
-                    <Form.Control type="phone" placeholder="+9 (999) 999 99 99" size={"lg"}/>
-                    <Form.Control type="text" placeholder="ФИО" size={"lg"}/>
-                    <Button variant="primary" size={"lg"}>Отправить</Button>
+                <p>
+                    Если вы хотите стать нашим партнером, инвестором или заказчиком, то просим вас заполнить данную форму.
+                </p>
+                <form className={styles.footerTopInput}>
+                    <Form.Control type="email" placeholder="Почта" size="lg" />
+                    <Form.Control type="phone" placeholder="+9 (999) 999 99 99" size="lg" />
+                    <Form.Control type="text" placeholder="ФИО" size="lg" />
+                    <Button variant="primary" size="lg">
+                        Отправить
+                    </Button>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Corporation;
--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,27 +1,24 @@
-import {HeaderImage} from "../../components/HeaderImage/HeaderImage.tsx";
-import './ProjectsPage.css'
-import {Project} from "../../components/Project/Project.tsx";
+import { HeaderImage } from '../../components/HeaderImage/HeaderImage.tsx';
+import { Project } from '../../components/Project/Project.tsx';
+import styles from './ProjectsPage.module.css';
 
+
+const imageUrl = 'public/image/projects-page-header.jpg';
+const title = 'Проекты';
+const description = 'Коммерческие проекты заказанные у SDU IT Park.';
 
 export const ProjectsPage = () => {
-    console.log("created ProjectsPage")
-    const imageUrl = 'public/image/projects-page-header.jpg'
-    const title = "Проекты"
-    const description = "Коммерческие проекты заказанные у SDU IT Park.";
+    console.log('created ProjectsPage');
 
     return (
-        <div className={"projects-page"}>
-            <HeaderImage
-                imageUrl={imageUrl}
-                title={title}
-                description={description}
-            />
-            <main id={'main'}>
-                <div className="content">
-                    <div className="content-title">Текущие проекты</div>
-                    <div className="project-list">
-                        <Project/>
-                        <Project/>
+        <div className={styles.projectsPage}>
+            <HeaderImage imageUrl={imageUrl} title={title} description={description} />
+            <main id="main">
+                <div className={styles.projectsPageContent}>
+                    <div className={styles.projectsPageContentTitle}>Текущие проекты</div>
+                    <div className={styles.projectList}>
+                        <Project />
+                        <Project />
                     </div>
                 </div>
             </main>
