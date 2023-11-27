920f703ce24e78cde1dbe64d9cdfa6119891e5a9
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-11-27 13:44:12 +0600

    fixed bug with router and added imageHeader for OfePage

--- src/Pages/EntrepreneurshipOffice/EntrepreneurshipOffice.tsx
+++ src/Pages/EntrepreneurshipOffice/EntrepreneurshipOffice.tsx
@@ -2,10 +2,16 @@ import AboutOFE from "../../components/About/AboutOFE.tsx";
 import OfficeActivity from "../../components/OfficeActivity/OfficeActivity.tsx";
 import OurGuests from "../../components/OurGuests/OurGuests.tsx";
 import Incubators from "../../components/Incubators/Incubators.tsx";
+import {HeaderImage} from "../../components/HeaderImage/HeaderImage.tsx";
 
 function EntrepreneurshipOffice() {
+    const imageUrl = 'public/image/ofe-page-header.jpeg'
+    const title = "Офис предпринимательства"
+    const description = "Офис предпринимательства СДУ был открыт в августе 2022 года, главной целью которого является создать предпринимательскую экосистему в Университете.";
+
     return (
         <>
+            <HeaderImage imageUrl={imageUrl} title={title} description={description} />
             <AboutOFE />
             {/* <Slider /> */}
             <OfficeActivity />
