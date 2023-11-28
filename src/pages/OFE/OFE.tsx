9eface384705857dbcbf542cfb01ce27dabac8f2
smilexah <smilexah@inbox.ru>
2023-11-28 12:15:32 +0600

    getting update from master branch

--- src/pages/OFE/OFE.tsx
+++ src/pages/OFE/OFE.tsx
@@ -1,12 +1,16 @@
+<<<<<<< HEAD
 <<<<<<< HEAD:src/pages/OFE/OFE.tsx
+=======
+>>>>>>> 5ddc8a6 (getting update from master branch)
 import AboutOFE from '../../components/About/AboutOFE.tsx';
 import OfficeActivity from '../../components/OfficeActivity/OfficeActivity.tsx';
 import OurGuests from '../../components/OurGuests/OurGuests.tsx';
-import Incubators from '../../components/Incubators/Incubators.tsx';
+import {Incubators} from '../../components/Incubators/Incubators.tsx';
 import {HeaderImage} from "../../components/HeaderImage/HeaderImage.tsx";
 import Slider from "../../components/SliderPart/Slider.tsx";
 import Contacts from "../../components/Contacts/Contacts.tsx";
 import styles from "./OFE.module.css";
+<<<<<<< HEAD
 =======
 import AboutOFE from '../../Components/About/AboutOFE';
 import OfficeActivity from '../../Components/OfficeActivity/OfficeActivity';
@@ -17,6 +21,8 @@ import Slider from '../../Components/Slider/Slider';
 import Contacts from '../../Components/Contacts/Contacts';
 import styles from './OFE.module.css';
 >>>>>>> 2e46686 (in front of classNames added their class name like className__container and so on, and updated css file using this changes):src/Pages/OFE/OFE.tsx
+=======
+>>>>>>> 5ddc8a6 (getting update from master branch)
 
 function OFE() {
     const imageUrl =
