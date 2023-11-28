f17dfe2badbefdb2c6055a0312caed296a625f7d
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-11-28 17:49:04 +0600

    fix: header top, added new route

--- src/components/HeaderTop/HeaderTop.tsx
+++ src/components/HeaderTop/HeaderTop.tsx
@@ -5,13 +5,13 @@ const HeaderTop = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">SDU Teхнопарк</Navbar.Brand>
+                <Navbar.Brand href="/">SDU Teхнопарк</Navbar.Brand>
                 <Nav>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <NavDropdown title="SDU IT Park" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/">О нас, IT Park</NavDropdown.Item>
+                                <NavDropdown.Item href="/about-it-park">О нас, IT Park</NavDropdown.Item>
                                 <NavDropdown.Item href="/it-park-projects">Проект</NavDropdown.Item>
                                 <NavDropdown.Item href="/cources">Курсы</NavDropdown.Item>
                             </NavDropdown>
